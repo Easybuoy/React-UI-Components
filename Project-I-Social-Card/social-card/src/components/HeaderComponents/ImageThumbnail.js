dcefcc0fbaacf0cb981849d4b1c72cdadafec9ff
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-05-27 15:35:58 +0100

    add event-listener

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -5,6 +5,9 @@ function ImageThumbnail() {
   return (
     <div className="headerImageDiv">
       <img
+        onClick={() => {
+          window.location.reload();
+        }}
         className="headerImage"
         src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"
         alt="logo"
